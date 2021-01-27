73970f71218d1c65f18093e4b2cf5e9ce78f2393
yoctoalex <shemyakin@yoctocode.com>
2021-01-27 02:17:39 +0200

    Update pipeline script

--- eap_ec2_ansible/jenkins.groovy
+++ eap_ec2_ansible/jenkins.groovy
@@ -2,16 +2,25 @@ pipeline {
     agent any
 
     stages {
-        stage('Setup new instance') {
+        stage('Clone sources') {
+            steps {
+                git url: 'https://github.com/yoctoalex/f5-blogs.git'
+            }
+        }
+        stage('Install F5 ansible collection and requirements') {
+            steps {
+                sh "ansible-galaxy collection install f5devcentral.cloudservices -p ./eap_ec2_ansible/ansible/collections"
+                sh "pip3 install -r ./eap_ec2_ansible/ansible/collections/ansible_collections/f5devcentral/cloudservices/requirements.txt"
+                sh "pip3 install boto"
+            }
+        }
+        stage('Deploy F5 Essential App Protected environment') {
           steps {
-            dir("/var/f5blogs/eap_ec2_ansible/") {
               ansiblePlaybook([
-                inventory   : 'hosts.yml',
-                playbook    : './ansible/playbook.yml',
-                colorized   : true,
+                inventory   : './eap_ec2_ansible/hosts.yml',
+                playbook    : './eap_ec2_ansible/ansible/playbook.yml',
                 extras: '-vvv'
               ])
-            }
           }
         }
     }
